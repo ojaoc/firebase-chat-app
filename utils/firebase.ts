e223a8929d93ae1442feb38102684f83a9c57be8
João Correia <correia.jafonso@gmail.com>
2021-11-22 23:05:03 +0000

    fixed process not defined error

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -2,15 +2,13 @@ import { initializeApp, FirebaseApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-const { env } = process;
-
 const config = {
-  apiKey: env.NEXT_PUBLIC_API_KEY,
-  authDomain: env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: env.NEXT_PUBLIC_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 const app: FirebaseApp = initializeApp(config);
