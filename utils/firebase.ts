2867f2736b7d9a27e0179781d561fd19e8a075e8
João Correia <correia.jafonso@gmail.com>
2021-11-22 22:56:03 +0000

    Updated Next.js, set firebase vars as env variables

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,14 +1,16 @@
-import { initializeApp, FirebaseApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { initializeApp, FirebaseApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const { env } = process;
 
 const config = {
-  apiKey: "AIzaSyDWYmSLMJO4LdOqPJqhtx4d50yGD5aX6pk",
-  authDomain: "simple-chat-app-35bfb.firebaseapp.com",
-  projectId: "simple-chat-app-35bfb",
-  storageBucket: "simple-chat-app-35bfb.appspot.com",
-  messagingSenderId: "539935026327",
-  appId: "1:539935026327:web:cc83260b1f8e2680260dd2",
+  apiKey: env.NEXT_PUBLIC_API_KEY,
+  authDomain: env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: env.NEXT_PUBLIC_APP_ID,
 };
 
 const app: FirebaseApp = initializeApp(config);
